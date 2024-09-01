9c272436592333882bbccf4e855057f58f557d2d
sairuk <sairuk@gmail.com>
2024-09-01 16:07:16 +1000

    initially hacky working code

---
+++ 0mhz_dosbox.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+#
+# hacked together by sairuk
+#
+
+import os, sys
+import configparser
+import argparse
+from lxml import etree
+
+dosbox_confdir = "0mhz_dosbox"
+
+class DosBox(object):
+    def __init__(self):
+        self.autorun = []
+        self.conf = configparser.ConfigParser()
+        return
+
+    def drive(self, id):
+        drive_map = {
+            '0': 'a',
+            '1': 'b',
+            '2': 'c',
+            '4': 'd',
+            '5': 'e',
+            '6': 'f',
+            '7': 'g'
+        }
+        if id in drive_map:
+            return drive_map[id]
+        return
+
+    def device(self, dev):
+        device_map = {
+            "vhd": "hdd",
+            "chd": "iso",
+            "img": "floppy"
+        }
+        
+        if dev in device_map:
+            return device_map[dev]
+        return None
+
+    def template(self):
+        self.conf["dos"] = { "ver": "7.0" }
+        self.conf["autoexec"] = {}
+    
+    def autoexec_cmds(self):
+        return
+
+def main(args):
+    dosbox = DosBox()
+    dosbox.template()
+
+    zmhz_path = args.mgls
+    dosbox_path = args.dosbox_base
+    
+    if not os.path.exists(zmhz_path):
+        print(f"Failed to find input folder: {zmhz_path}", file=sys.stderr)
+        exit(1)
+
+    if not os.path.exists(dosbox_confdir):
+        os.mkdir(dosbox_confdir)
+
+    for root, dirs, files in os.walk(zmhz_path):
+        for filename in files:
+            autoexec = []
+            boot = "c"
+            bootlook = True
+            autoexec_cmd = ""
+            abs_path = os.path.join(root, filename)
+            conf_dosbox = os.path.join(dosbox_confdir,os.path.splitext(filename)[0] + ".conf")
+
+            try:
+                xml = etree.parse(abs_path)
+            except etree.XMLSyntaxError:
+                print(f"Invalid XML: {filename}", file=sys.stderr)
+                next
+            elements = xml.getroot().xpath("//file")
+            dev_dosbox = None
+            for element in elements:
+                index = element.attrib["index"]
+                file_ao486 = element.attrib["path"]
+                filetype = os.path.splitext(file_ao486)[-1][1:]
+                devtype_ao486 = element.attrib["type"]
+                dev_dosbox = dosbox.device(filetype)
+                drv = dosbox.drive(index)
+                if dev_dosbox is None:
+                    print(f"Failed to lookup device map for: {filetype} in {filename} ({abs_path})", file=sys.stderr)
+                    break
+                autoexec.append(f'imgmount {drv} "{dosbox_path}/{file_ao486}" -t {dev_dosbox}')
+
+                if index == "0" or index == "1":
+                    print(f"Game {filename} has a floppy type image at index {index} if you have trouble booting this may be why", file=sys.stderr)
+
+                # Wizardry I didn't like this
+                #if bootlook and index == "0":
+                #    boot = dosbox.drive(index)
+                #    bootlook = False
+
+            if dev_dosbox is None:
+                next
+
+            autoexec.append(f'boot -l {boot} \n')
+            autoexec_cmd = '\n'.join(autoexec)
+
+            # write config
+            with open(conf_dosbox, 'w') as d:
+                dosbox.conf.write(d)
+                d.write(autoexec_cmd)
+            #break
+
+    return
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Convert 0mhz A0486 mgl files to dosbox configs.')
+
+    parser.add_argument('--mgls', type=str, help='path to 0mhz AO486 mgls for processing', required=True)
+    parser.add_argument('--dosbox-base', type=str, help='dosbox base part, where will dosbox see the 0mhz vhd/chd/img etc files', required=True)
+
+    args = parser.parse_args()
+    main(args)
--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # 0mhz_dosbox
+
 basic convertor for the 0mhz collection to dosbox-x
+
+requires dosbox-x atm for vhd geometry support and dosbox reported version must be at least 7.0
+
+
+example run for conversion
+```
+ython3 0mhz_dosbox.py --mgls "/mnt/retrodump/retronas/mister/_DOS Games" --dosbox-base "/mnt/retrodump/retronas/mister/games/AO486"
+```
+
+will ignore invalid xml, e.g.
+```
+Invalid XML: Command & Conquer Red Alert.mgl
+Invalid XML: Joe & Mac - Caveman Ninja.mgl
+Invalid XML: Command & Conquer.mgl
+Invalid XML: Warcraft - Orcs and Humans.mgl
+```
+
+will report on games with and `index` < 2, if the game works fine you can ignore this. e.g. Wizardry requires a scenario disk in `A:` but still boots from the VHD on `C:`
+```
+Game Wizardry IV.mgl has a floppy type image at index 0 if you have trouble booting this may be why
+Game Wizardry V.mgl has a floppy type image at index 0 if you have trouble booting this may be why
+Game Wizardry III.mgl has a floppy type image at index 0 if you have trouble booting this may be why
+Game Wizardry I.mgl has a floppy type image at index 0 if you have trouble booting this may be why
+Game Wizardry II.mgl has a floppy type image at index 0 if you have trouble booting this may be why
+```
+
+output will be a barebones dosbox config file per game you can load with `-conf`
+```
+$ cat 0mhz_dosbox/Discworld.conf 
+[dos]
+ver = 7.0
+
+[autoexec]
+
+imgmount c "/mnt/retrodump/retronas/mister/games/AO486/media/discworld/discworld.vhd" -t hdd
+imgmount d "/mnt/retrodump/retronas/mister/games/AO486/media/discworld/discworld.chd" -t iso
+boot -l c 
+```
+
+Original MisTer `.mgl` contents
+$ cat Discworld.mgl 
+```
+mistergamedescription>
+    <rbf>_computer/ao486</rbf>
+    <file delay="0" type="s" index="2" path="media/discworld/discworld.vhd"/>
+    <file delay="0" type="s" index="4" path="media/discworld/discworld.chd"/>
+    <reset delay="1"/>
+</mistergamedescription>
+```
+
+load in DosBox-X flatpak instance
+```
+flatpak run com.dosbox_x.DOSBox-X -conf 'Wizardry II.conf'
+```
